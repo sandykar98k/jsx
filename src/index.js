ce2bac132c684319a0317c8658de922fc0569b8c
sandykar98k <iambansalsandeep@gmail.com>
2018-12-17 21:38:55 +0530

    Jsx Into

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+// Import the React and ReactDOM libraries.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component.
+const App = () => {
+  const buttonText = 'Click Me';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // This is JSX and it converts into normal javaScript(This is done by babel).
+  return (
+    <div>
+      <label htmlFor="name">Enter Your Name:</label>
+      <input type="text" id="name" />
+      <button>{buttonText}</button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen.
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// NOTE: why import not require?
+// import statement confirm the (ES2015 module system).
+// while the require confirm the (CommonJS modules).
+
+// NOTE: JSX is almost identical to HTML with some tiny differences.
